f36fa5119c5a1ef71aee27b9144e57852858ca6b
Ivan Zabrovskiy <loriowar@gmail.com>
2013-07-07 15:42:44 +0400

    Work version v0.1.5
     - Add button for detect free alias
     - Some modifications in css

---
+++ app/assets/javascripts/check_button.js
@@ -0,0 +1,22 @@
+function bindClickEvent(btn_id, short_url_id) {
+    jQuery('#' + btn_id).on('click', function() {
+        var short_url = jQuery('input#' + short_url_id).val();
+        if(jQuery('#big_url_vc_short_url').val().length) {
+            jQuery.post(
+                '/',
+                {
+                    check_alias: true,
+                    vc_short_url: short_url
+                }
+            );
+        }
+    });
+};
+
+// TODO: make event on field in any case (now event fire only after remove focus)
+function bindChangeEvent(short_url_id) {
+    var element_id = 'input#' + short_url_id;
+    jQuery(element_id).on('change', function() {
+        jQuery(element_id).removeClass('border_red').removeClass('border_green');
+    });
+};
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,17 @@
 class HomeController < BaseController
 
   def create
-    @big_url = BigUrl.create(params[:big_url])
+    if params[:check_alias].present?
+      @alias_exist = BigUrl.find_by_alias(params[:vc_short_url]).present?
+    else
+      @big_url = BigUrl.create(params[:big_url])
 
-    unless @big_url.errors.any?
-      # Fill notice with result url
-      flash.now[:notice] = 'http://' << (request.host == 'localhost' ? request.host_with_port : request.host) << '/' << @big_url.vc_short_url
-      # Clear variable after success save
-      @big_url = nil
+      unless @big_url.errors.any?
+        # Fill notice with result url
+        flash.now[:notice] = 'http://' << (request.host == 'localhost' ? request.host_with_port : request.host) << '/' << @big_url.vc_short_url
+        # Clear variable after success save
+        @big_url = nil
+      end
     end
 
     respond_to do |format|
