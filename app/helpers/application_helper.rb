3d73349f8338b1348d5fba7cbcbc9583428558a5
Ivan Zabrovskiy <loriowar@gmail.com>
2013-12-03 12:25:45 +0400

    Head menu
     - Add head menu with locales selector
     - Create views for simple addition new locales
     - Add sprite for flags icons
     - Add directory for static css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def get_current_url_with_locale(locale)
+    current_url = request.original_url
+    if current_url =~ /locale=[a-z]{2}/
+      current_url.gsub(/locale=[a-z]{2}/, "locale=#{locale}")
+    elsif current_url.include?('?')
+      "#{current_url}&locale=#{locale}"
+    else
+      "#{current_url}?locale=#{locale}"
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,10 @@ module CutURL
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # config.assets.prefix = "/assets"
+    config.assets.paths << "#{Rails.root}/public/images"
+    config.assets.paths << "#{Rails.root}/public/stylesheets"
+    config.assets.paths << "#{Rails.root}/public/javascripts"
   end
 end
