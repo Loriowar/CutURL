60f69be86911a83d74f2b7b2bbfee6287cc1b0ae
Ivan Zabrovskiy <loriowar@gmail.com>
2013-12-04 10:09:21 +0400

    Small fix
     - Add jquery-turbolink for future events
     - In href for locale url replace on path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def get_current_url_with_locale(locale)
-    current_url = request.original_url
+  def get_current_path_with_locale(locale)
+    current_url = request.fullpath
     if current_url =~ /locale=[a-z]{2}/
       current_url.gsub(/locale=[a-z]{2}/, "locale=#{locale}")
     elsif current_url.include?('?')
